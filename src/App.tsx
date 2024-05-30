e407be742c13039bc2204c2bbf3b7f155aabb4b6
geethu312 <160865783+geethu312@users.noreply.github.com>
2024-05-30 11:17:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Subscription from "./Pages/Subscription/Subscription";
 import Login from "./Pages/Login/Login";
-import SubsData from "./Pages/SubsData/SubsData";
 
 function App() {
   return (
